c1a5bf1d5885dcbe1b4b3eda76fbc04568e5d392
zakariae_elhajri <zakriaelhajri@gmail.com>
2022-12-25 19:27:52 +0100

    zakariae is connected

--- assignment/settings.py
+++ assignment/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django-project',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }

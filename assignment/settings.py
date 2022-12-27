f9e6f10d9ef8024d701fa303f5e458da5eeb6fa2
Abdessamad Pas <boughadoin@gmail.com>
2022-12-27 04:25:37 +0100

    fix db container

--- assignment/settings.py
+++ assignment/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST':     os.environ.get('DB_HOST'),
         'PORT':     os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 

2393eb298cfe7e9c0434b4619d3ba5e48871a30a
zakariae_elhajri <zakriaelhajri@gmail.com>
2023-01-05 00:46:54 +0100

    fixe interfaces

--- assignment/settings.py
+++ assignment/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/templates')
+    os.path.join(BASE_DIR, 'assignment/static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 

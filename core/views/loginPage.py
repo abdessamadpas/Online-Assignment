0a8e38fca6abb436d9c7326480392331b95f2551
AbdessamadPas <boughadoin@gmail.com>
2023-01-14 17:13:44 +0100

    fix parts of project

--- core/views/loginPage.py
+++ core/views/loginPage.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
-from core.forms import LoginForm
+#from core.forms import LoginForm
 from django.http import HttpResponseRedirect
 
 # Create your views here.

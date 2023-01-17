1e5e7fd76c10de1f1385f1fe3ebb7ce539795407
AbdessamadPas <boughadoin@gmail.com>
2023-01-17 18:00:15 +0100

    start work in exam section part of student

--- core/models/matiere.py
+++ core/models/matiere.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Matiere(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=300,null=True)
     
--- core/views_etudiant/passer_qcm.py
+++ core/views_etudiant/passer_qcm.py
@@ -7,17 +7,19 @@ from core.models import *
 @login_required(login_url='login')
 def passer_qcm(request, module_id, exam_id):
     user = request.user
-    print("dashboard admin  page user is",user)
+    print("passer qcm   page user is",user)
     #! check if user is student or not to access this page
     print("dashboard admin  page user is",user)
     if user.is_staff:
         return redirect('access_denied_student')
 
     exam = get_object_or_404(Exam, id=exam_id)
-
+    questions = Question.objects.filter(exam_id = exam.id ).prefetch_related('answer_set')
+    print("questions",questions)    
     context = {
         'exam': exam,
         'module_id': module_id,
         'exam_id': exam_id,
+        'questions': questions,
     }
     return render(request, 'etudiant/pages/passer_qcm.html', context)

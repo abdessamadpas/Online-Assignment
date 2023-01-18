10745106b617f2b003cfbb3a4e6485e49688db74
chaimae_loukili <c.loukili1999@gmail.com>
2023-01-18 23:07:56 +0100

    make admin header,profile,setting dynamic

--- core/views/dashboard.py
+++ core/views/dashboard.py
@@ -9,6 +9,7 @@ from core.models.auth.profile import Profile
 from core.views.auth.user_checks import staff_check
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 @user_passes_test(staff_check, login_url='login')
@@ -24,10 +25,16 @@ def dashboard(request):
     exams_total = Exam.objects.all().count()
     students_total = Student.objects.all().count()
     template = loader.get_template('admine/pages/index.html')
+    profile = Profile.objects.get(user=request.user)
+    user_id=User.objects.get(username = user)
+    email = user_id.email
     context = {
         'models_total': models_total,
         'exams_total': exams_total,
         'students_total': students_total,
+        'profile': profile ,
+        'user': user_id,
+        'email': email
     }   
 
     return HttpResponse(template.render(context, request))

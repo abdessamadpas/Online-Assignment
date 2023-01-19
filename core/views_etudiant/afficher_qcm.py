921365aadf6382f64e90058f2b3c9d6de2e98232
AbdessamadPas <boughadoin@gmail.com>
2023-01-19 13:30:38 +0100

    done with results in student UI

--- core/views_etudiant/afficher_qcm.py
+++ core/views_etudiant/afficher_qcm.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 
 from core.models.exam import Exam
 # Create your views here.
--- core/views_etudiant/resultat_qcm.py
+++ core/views_etudiant/resultat_qcm.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from core.models import *
 # Create your views here.
 
 @login_required(login_url='login')
@@ -8,4 +10,19 @@ def resultat_qcm(request):
     print("dashboard admin  page user is",user)
     if user.is_staff:
         return redirect('access_denied_student')
-    return render(request, 'etudiant/pages/resultat_qcm.html')
+    results = Attempter.objects.filter(user=user).order_by('-create_at')
+    exam_name = Attempter.objects.filter(user=user).first().exam
+    exam_id=exam_name.id
+    exam_selected = Exam.objects.get(id=exam_id)
+    matiere_id = exam_selected.matiere_id.id
+    matiere = Matiere.objects.get(id=matiere_id).name
+    print("matiere",matiere)
+    context = {
+        "results": results,
+        "matiere": matiere,
+        "exam": exam_name,
+        "user": user,
+
+    }
+
+    return render(request, 'etudiant/pages/resultat_qcm.html', context)
